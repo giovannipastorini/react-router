8f6cb30462f1c2752aeacde8356486652bb701c5
giovannipastorini <giovannipastorini9@gmail.com>
2026-05-18 20:40:36 +0200

    Fetch and display products in ProductsPage; implement product listing with cards

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,12 +1,48 @@
-
+import { useEffect, useState } from "react"
 
 export default function ProductsPage (){
+
+    const [products, setProducts]=useState([])
+
+    useEffect(
+        ()=>{
+            //console.log("pagina prodotti caricata");
+        const api_url="https://fakestoreapi.com/products";
+        fetch(api_url)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                
+            // qui salvi i dati nello stato
+            setProducts(data);
+            
+        })
+        
+    }
+    ,[])
+    //console.log(products);
 
     return(
         <>
             
             <div className="container">
                 <h1>Prodotti</h1>
+                <div className="products-list">
+                    <div className="row">
+                    </div>
+                    
+                        {
+                            products.map(product =>(
+                                <div className="col-4" key={product.id}>
+                                    <div className="card">
+                                        <p>{product.title}</p>
+                                    </div>
+                                </div>
+                                
+                            ))
+                        }
+                    
+                </div>
             </div>
         </>
     )
